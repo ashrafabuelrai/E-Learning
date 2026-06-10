8523c05162daf9bdb075745d6899816c72a20ae9
Ashraf Abu Elrai <126774761+ashrafabuelrai@users.noreply.github.com>
2026-06-10 10:49:02 +0300

    Edit Repository

--- E-Learning.Infrastructure/Repositories/Repository.cs
+++ E-Learning.Infrastructure/Repositories/Repository.cs
@@ -41,7 +41,7 @@ namespace E_Learning.Infrastructure.Repositories
                     query = query.Include(prop);
                 }
             }
-            return await query.FirstOrDefaultAsync();
+            return await query.FirstOrDefaultAsync(filter);
         }
 
         public async Task<IQueryable<T>> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProp = null)
